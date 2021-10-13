10b34bcf6cef5da3574c69fec47054c0e2e5cf43
Jochen <63775720+JochenFalk@users.noreply.github.com>
2021-10-13 17:14:25 +0200

    Fix breaking bug to Inheritance
    - Comment out table assignment for Student and Instructor
    - Added HasDiscriminator property to Person table
    
    Note: The app was broken because Fluent API was not using the Discriminator.

--- ContosoUniversity/ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
@@ -22,15 +22,16 @@ namespace ContosoUniversity.Data
         {
             modelBuilder.Entity<Course>().ToTable("Course");
             modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
-            modelBuilder.Entity<Student>().ToTable("Student");
+            //modelBuilder.Entity<Student>().ToTable("Student");
             modelBuilder.Entity<Department>().ToTable("Department");
-            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            //modelBuilder.Entity<Instructor>().ToTable("Instructor");
             modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
             modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
             modelBuilder.Entity<Person>().ToTable("Person");
+            modelBuilder.Entity<Person>().HasDiscriminator<string>("Discriminator");
 
             modelBuilder.Entity<CourseAssignment>()
-                .HasKey(c => new { c.CourseID, c.InstructorID });
+                .HasKey(c => new { c.CourseID, c.InstructorID });            
         }
     }
 }
