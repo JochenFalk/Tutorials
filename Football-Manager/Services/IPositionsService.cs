b754629d0e173cefd0900167d0c2f42e027b3e2d
Jochen <63775720+JochenFalk@users.noreply.github.com>
2021-10-19 16:32:24 +0200

    Created app services
    - Created services for Players and Positions

---
+++ Football-Manager/Services/IPositionsService.cs
@@ -0,0 +1,13 @@
+﻿using Football_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Football_Manager.Services
+{
+    public interface IPositionsService
+    {
+        Task<IEnumerable<Position>> GetPositionsList();
+    }
+}
---
+++ Football-Manager/Services/PositionsService.cs
@@ -0,0 +1,27 @@
+﻿using Football_Manager.Data;
+using Football_Manager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Football_Manager.Services
+{
+    public class PositionsService : IPositionsService
+    {
+        private readonly FootballDbContext _context;
+
+        public PositionsService(FootballDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Position>> GetPositionsList()
+        {
+            return await _context.Positions
+                .OrderBy(x => x.DisplayOrder)
+                .ToListAsync();
+        }
+    }
+}
