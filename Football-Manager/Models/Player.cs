4d15bd4dd967860cd7c3e004bb43b95932e4b691
Jochen <63775720+JochenFalk@users.noreply.github.com>
2021-10-19 16:21:12 +0200

    Started tutorial: Full Stack CRUD using Angular 8 and ASP.NET Core 5 Web API
    - Started: Full Stack CRUD using Angular 8 and ASP.NET Core 5 Web API
    - Create new project and scaffold DB from SQL server into Models
    - Specified the connection string in appsettings.json
    - Set default connection in ConfigureServices method of Startup.cs

---
+++ Football-Manager/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Football_Manager.Models
+{
+    public partial class Player
+    {
+        public int Id { get; set; }
+        public int? ShirtNo { get; set; }
+        public string Name { get; set; }
+        public int? PositionId { get; set; }
+        public int? Appearances { get; set; }
+        public int? Goals { get; set; }
+    }
+}
---
+++ Football-Manager/Models/Position.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Football_Manager.Models
+{
+    public partial class Position
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? DisplayOrder { get; set; }
+    }
+}
