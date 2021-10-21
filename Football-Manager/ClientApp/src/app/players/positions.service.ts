3b0eca451f7686329261f73373ed82b94576ac86
Jochen <63775720+JochenFalk@users.noreply.github.com>
2021-10-21 14:23:09 +0200

    Implemented interface and services
    - Implemented interface and services for Player and Position

--- Football-Manager/ClientApp/src/app/players/positions.service.ts
+++ Football-Manager/ClientApp/src/app/players/positions.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Position } from "./position";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PositionsService {
 
-  constructor() { }
+  private apiURL = "http://localhost:44333/api";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getPositions(): Observable<Position[]> {
+    return this.httpClient.get<Position[]>(this.apiURL + '/positions')
+      .pipe(
+        catchError(this.errorHandler)
+      );
+  }
+
+  errorHandler(error) {
+    let errorMessage = '';
+
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
 }
