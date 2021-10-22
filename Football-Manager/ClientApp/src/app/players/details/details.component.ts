39293fab334ebd135cde16bf8e7a1488487d7fb4
Jochen <63775720+JochenFalk@users.noreply.github.com>
2021-10-22 09:27:37 +0200

    Implemented Player Details Component

--- Football-Manager/ClientApp/src/app/players/details/details.component.ts
+++ Football-Manager/ClientApp/src/app/players/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Player } from "../player";
+import { PlayersService } from "../players.service";
 
 @Component({
   selector: 'app-details',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  player: Player;
 
-  ngOnInit() {
+  constructor(
+    public playersService: PlayersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['playerId'];
+    this.playersService.getPlayer(this.id).subscribe((data: Player) => {
+      this.player = data;
+    });
   }
-
 }
